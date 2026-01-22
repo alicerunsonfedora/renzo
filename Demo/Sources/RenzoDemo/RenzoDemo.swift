fb888075c23fa16a9b0b86f746e6b08a24a08c23
Marquis Kurt <software@marquiskurt.net>
2026-01-22 18:21:02 -0500

    Update PlaydateKit

--- Demo/Sources/RenzoDemo/RenzoDemo.swift
+++ Demo/Sources/RenzoDemo/RenzoDemo.swift
@@ -1,23 +1,7 @@
 import PlaydateKit
 import Renzo
 
-/// Boilerplate entry code
-nonisolated(unsafe) var game: Game!
-@_cdecl("eventHandler") func eventHandler(
-    pointer: UnsafeMutablePointer<PlaydateAPI>,
-    event: System.Event,
-    arg _: CUnsignedInt
-) -> CInt {
-    switch event {
-    case .initialize:
-        Playdate.initialize(with: pointer)
-        game = Game()
-        System.updateCallback = game.update
-    default: game.handle(event)
-    }
-    return 0
-}
-
+@PlaydateMain
 final class Game: PlaydateGame {
     let renderer: SceneRenderer
 
