a234e0d42a118ec3729f40c80e206a2490a0c099
Marquis Kurt <software@marquiskurt.net>
2026-01-31 00:49:35 -0500

    Re-add string extensions (for now)

---
+++ Sources/Renzo/Extensions/String.swift
@@ -0,0 +1,10 @@
+
+extension String {
+    package static func == (lhs: String, rhs: String) -> Bool {
+        lhs.utf8.elementsEqual(rhs.utf8)
+    }
+
+    package static func != (lhs: String, rhs: String) -> Bool {
+        !lhs.utf8.elementsEqual(rhs.utf8)
+    }
+}
