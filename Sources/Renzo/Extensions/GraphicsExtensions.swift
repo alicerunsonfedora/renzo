7be70bbefe5dad45c0af6bf9a4fcf08440855e13
Marquis Kurt <software@marquiskurt.net>
2026-02-01 15:12:56 -0500

    Use PGColor.dithered(by:) from PDGraphics

--- Sources/Renzo/Extensions/GraphicsExtensions.swift
+++ Sources/Renzo/Extensions/GraphicsExtensions.swift
@@ -8,17 +8,6 @@
 import PDGraphics
 import PlaydateKit
 
-let bayer: [[UInt8]] = [
-    [0, 32, 8, 40, 2, 34, 10, 42],
-    [48, 16, 56, 24, 50, 18, 58, 26],
-    [12, 44, 4, 36, 14, 46, 6, 38],
-    [60, 28, 52, 20, 62, 30, 54, 22],
-    [3, 35, 11, 43, 1, 33, 9, 41],
-    [51, 19, 59, 27, 49, 17, 57, 25],
-    [15, 47, 7, 39, 13, 45, 5, 37],
-    [63, 31, 55, 23, 61, 29, 53, 21],
-]
-
 extension Graphics {
     /// Draws a filled triangle at the points provided by the face.
     ///
@@ -44,28 +33,5 @@ extension Graphics {
         Graphics.drawLine(Line(start: triangle.pointA, end: triangle.pointB), lineWidth: lineWidth)
         Graphics.drawLine(Line(start: triangle.pointA, end: triangle.pointC), lineWidth: lineWidth)
         Graphics.drawLine(Line(start: triangle.pointC, end: triangle.pointB), lineWidth: lineWidth)
-    }
-}
-
-extension Graphics.Color {
-    public static func dithered(by opacity: Float) -> Graphics.Color {
-        var pattern: [UInt8] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        let threshold = UInt8((1 - opacity) * 64)
-        for row in 0..<8 {
-            for col in 0..<8 {
-                if bayer[row][col] >= threshold {
-                    pattern[row] |= (1 << col)  // set
-                } else {
-                    pattern[row] &= ~(1 << col)  // clear
-                }
-            }
-        }
-        return .pattern(
-            (
-                pattern[0], pattern[1], pattern[2], pattern[3], pattern[4], pattern[5], pattern[6],
-                pattern[7]
-            ),
-            mask: (255, 255, 255, 255, 255, 255, 255, 255)
-        )
     }
 }
