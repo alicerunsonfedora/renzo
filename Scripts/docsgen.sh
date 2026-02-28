c63e2ccad96715b51ea5dafe385f600cf5d41efb
Marquis Kurt <software@marquiskurt.net>
2026-02-27 22:25:43 -0500

    Set up docs via GitHub Pages
    This is the best I can think of at the moment because it seems all
    the static site hosting options only really accept GitHub...
    
    Signed-off-by: Marquis Kurt <software@marquiskurt.net>

---
+++ Scripts/docsgen.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -e
+set -o pipefail
+
+mkdir -p /tmp/derivedData /tmp/docc
+
+xcodebuild docbuild -scheme Renzo \
+  -configuration Release \
+  -derivedDataPath /tmp/derivedData \
+  -destination 'platform=macOS' \
+  -toolchain "swift latest" \
+  -skipPackagePluginValidation \
+  -skipMacroValidation;
+
+xcrun docc merge $(find /tmp/derivedData -type d -name 'Renzo*.doccarchive') \
+  --synthesized-landing-page-name Renzo \
+  --synthesized-landing-page-kind SDK \
+  --output-path /tmp/docc/RenzoSDK.doccarchive
+
+$(xcrun -f docc -toolchain "swift latest") process-archive \
+  transform-for-static-hosting /tmp/docc/RenzoSDK.doccarchive \
+  --hosting-base-path . \
+  --output-path docs;
+
+echo "<script>window.location.href += \"/documentation\"</script>" > docs/index.html;
+
+rm -rf /tmp/derivedData /tmp/docc
